d9bad8ea883575692cd273a8356fcddaf332e812
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 <dannykill4221x@gmail.com>
2023-03-13 18:10:20 -0300

    update file name

--- GameHub/obj/Debug/net6.0/GameHub.AssemblyInfo.cs
+++ GameHub/obj/Debug/net6.0/GameHub.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("GameHub")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("gameHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("GameHub")]
-[assembly: System.Reflection.AssemblyTitleAttribute("GameHub")]
+[assembly: System.Reflection.AssemblyProductAttribute("gameHub")]
+[assembly: System.Reflection.AssemblyTitleAttribute("gameHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
